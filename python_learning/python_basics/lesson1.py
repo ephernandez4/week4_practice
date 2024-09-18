576c3d9fd498fe08f1ebb2e1ab833935a3594143
Hernandez <ephernandez4@cps.k12.il.us>
2024-09-18 15:09:04 -0500

    first commit

---
+++ python_learning/python_basics/lesson1.py
@@ -0,0 +1,24 @@
+#mkdir means make directory 
+#cd means change directory
+#ls means list directory
+#pwd means print working directory
+#touch means create a file
+#rm means remove a file
+# pound sign makes a comment
+# print ("Hello World!")
+# print ("Emily")
+# print ("age\t 17")
+# #/t means tab
+# print ("address")
+#press control + question mark to comment out multiple lines
+name = input ("What is your name?")
+print ("Hello," + name)
+age = input ("How old are your?")
+print ("You are " +  age  + " years old")
+born = input ( "Place of Birth?")
+print ("You were born in " + born )
+home = input ("What is your Address?")
+print ("You live at " + home)
+school = input ("What school did you attend")
+print ("You attended " + school)
+print ("Hello " + name + ", You are " + age + " years old. Your were born in" + born + "You live at " + home)
---
+++ python_learning/week4/challenge.py
@@ -0,0 +1,7 @@
+name=input("What is your name?")
+print("Your name is "+ name)
+income=input("How much did you make today?")
+comisson=(int(income)*13/100)
+print("Your name is "+ name + " and you collected " + str(comisson)+ " dollars.")
+#Emily H
+#Itzel G
